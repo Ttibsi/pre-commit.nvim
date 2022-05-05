3ac02e1020ce751cde59c678805cda8dbc66f92f
Ttibsi <ashisbitt@icloud.com>
2022-05-05 16:15:46 +0100

    Execution validation

--- lua/precommit/init.lua
+++ lua/precommit/init.lua
@@ -1,9 +1,16 @@
 -- :lua require("precommit").PreCommit()
 local function PreCommit()
-    print('hello pre-commit')
-
+ 
     -- Check if pre-commit is installed
         -- If not, give user an error
+    if os.execute("python3 -m pip list | grep pre-commit") ~= 0 then
+        print("Pre-commit is not installed")
+    elseif os.execute("git ls-files | grep pre-commit") == 256 then
+        print("No pre-commit-config found")
+    else
+        print('hello pre-commit')
+    end
+
     -- Save current buffer
     -- Open a new vertical split
     -- Run pre-commit in the new split and display output
