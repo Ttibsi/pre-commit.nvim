ffb7d7f68bd16a8b22a51223908dd6847f334457
Ttibsi <ashisbitt@icloud.com>
2022-06-01 14:53:37 +0100

    reload buffer

--- lua/precommit/init.lua
+++ lua/precommit/init.lua
@@ -1,7 +1,7 @@
 -- :lua require("precommit").PreCommit()
 local function PreCommit()
     local cmd = "pre-commit run --files %"
- 
+
     -- Check if pre-commit is installed
         -- If not, give user an error
     if os.execute("python3 -m pip list | grep pre-commit") ~= 0 then
@@ -20,9 +20,11 @@ local function PreCommit()
 
     -- Run pre-commit in the new split and display output
         -- ideally with colour
-    vim.api.nvim_call_function('termopen', {cmd})
+    vim.api.nvim_call_function('termopen ', {cmd})
 
     -- Reload buffer if needed
+    vim.api.nvim_command('e %')
+
 end
 
 return {
